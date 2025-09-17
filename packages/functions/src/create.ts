379019cd1662ee9fd890d627089494ac8b502539
Kavya Mothukuri <kavya@Mac-1321.lan>
2025-09-17 18:28:51 -0400

    Adding the API

---
+++ packages/functions/src/create.ts
@@ -0,0 +1,34 @@
+import * as uuid from "uuid";
+import { Resource } from "sst";
+import { Util } from "@notes/core/util";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { APIGatewayProxyEvent } from "aws-lambda";
+
+const dynamoDb = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+
+export const main = Util.handler(async (event: APIGatewayProxyEvent) => {
+  let data = {
+    content: "",
+    attachment: "",
+  };
+
+  if (event.body != null) {
+    data = JSON.parse(event.body);
+  }
+
+  const params = {
+    TableName: Resource.Notes.name,   // ✅ correct usage
+    Item: {
+      userId: "123",                  // stubbed userId for now
+      noteId: uuid.v1(),              // unique id
+      content: data.content,
+      attachment: data.attachment,
+      createdAt: Date.now(),
+    },
+  };
+
+  await dynamoDb.send(new PutCommand(params));
+
+  return JSON.stringify(params.Item);
+});
