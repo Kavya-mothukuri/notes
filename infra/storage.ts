f09c4424f089cbeade321cf2af801ae27b1b8360
Kavya Mothukuri <kavya@Mac-1610.lan>
2025-09-19 08:12:40 -0400

    fixing errors

--- infra/storage.ts
+++ infra/storage.ts
@@ -6,12 +6,14 @@ export const table = new sst.aws.Dynamo("Notes", {
     },
     primaryIndex: { hashKey: "userId", rangeKey: "noteId" },
   });
-  new sst.aws.Bucket("Uploads", {
+  
+
+  export const bucket = new sst.aws.Bucket("Uploads", {
     cors: {
       allowMethods: ["GET"]
     }
   });
-
-  export const bucket = new sst.aws.Bucket("Uploads");
+  
+  
   // Create a secret for Stripe
 export const secret = new sst.Secret("StripeSecretKey");
