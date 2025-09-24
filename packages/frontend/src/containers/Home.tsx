40aeb595cca557d531b8d47cc6420dea9d491d00
Kavya Mothukuri <kavya@Mac-2881.lan>
2025-09-24 11:47:18 -0400

    name

--- packages/frontend/src/containers/Home.tsx
+++ packages/frontend/src/containers/Home.tsx
@@ -70,7 +70,7 @@ export default function Home() {
   function renderLander() {
     return (
       <div className="lander">
-        <h1>Home</h1>
+        <h1>Nodoc</h1>
         <p className="text-muted">A simple note taking app</p>
       </div>
     );
